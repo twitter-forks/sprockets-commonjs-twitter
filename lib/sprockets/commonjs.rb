2845f7ef618bf695147c65e16e62dae1c4108772
Michael Jackson <mjijackson@gmail.com>
2012-09-25 11:56:53 -0700

    Add CommonJSExportsTemplate
    Also, rename CommonJS to CommonJSTemplate

--- lib/sprockets/commonjs.rb
+++ lib/sprockets/commonjs.rb
@@ -2,7 +2,7 @@ require 'sprockets'
 require 'tilt'
 
 module Sprockets
-  class CommonJS < Tilt::Template
+  class CommonJSTemplate < Tilt::Template
     self.default_mime_type = 'application/javascript'
 
     WRAPPER = <<-JAVASCRIPT.gsub(/\s+/, '')
@@ -17,6 +17,10 @@ module Sprockets
       'this.require'
     end
 
+    def self.wrap(path, data, namespace=default_namespace)
+      WRAPPER % [namespace, path, data]
+    end
+
     def prepare
       @namespace = self.class.default_namespace
     end
@@ -27,7 +31,7 @@ module Sprockets
       if wrap?(scope)
         scope.require_asset 'sprockets/commonjs'
         path = scope.logical_path.chomp('.module').inspect
-        WRAPPER % [namespace, path, data]
+        self.class.wrap(path, data, namespace)
       else
         data
       end
@@ -38,6 +42,33 @@ module Sprockets
     end
   end
 
-  register_postprocessor 'application/javascript', CommonJS
+  register_postprocessor 'application/javascript', CommonJSTemplate
   append_path File.expand_path('../..', __FILE__)
+
+  # This class can be used to wrap the contents of a file in a
+  # string that is exported from a CommonJS module.
+  #
+  # Add it to your Sprockets environment:
+  #
+  #   sprockets_env.register_engine '.mustache', Sprockets::CommonJSExportsTemplate
+  #
+  # Require a file with the appropriate extension:
+  #
+  #   //= require my_template
+  #
+  # Then, in your JavaScript:
+  #
+  #   var template = require('my_template');
+  class CommonJSExportsTemplate < CommonJSTemplate
+    self.default_mime_type = 'application/javascript'
+
+    def evaluate(scope, locals, &block)
+      @data = "module.exports = #{data.to_json}"
+      super
+    end
+
+    def wrap?(scope)
+      true
+    end
+  end
 end
